4ad9f3ac40699f49fefaa4419de38e04fb2c886f
Rusbelllozano <ruslllozano@gmail.com>
2019-03-13 18:14:23 -0500

    Add imgs and change styles

--- src/UIcomponents/cardinfofigi.js
+++ src/UIcomponents/cardinfofigi.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 
-import photo1 from '../assets/images/Foto1.png';
-import photo2 from '../assets/images/Foto2.png';
-import photo3 from '../assets/images/Foto3.png';
-import photo4 from '../assets/images/Foto4.png';
-import photo5 from '../assets/images/Foto5.png';
-import photo6 from '../assets/images/Foto6.png';
-import photo7 from '../assets/images/Foto7.png';
-import photo8 from '../assets/images/Foto8.png';
-import photo9 from '../assets/images/Foto9.png';
-import photo10 from '../assets/images/Foto10.png';
-import photo11 from '../assets/images/Foto11.png';
+import Card1 from '../assets/images/Card1.png';
+import Card1_1 from '../assets/images/Card1-1.png';
+import CardInfo1 from '../assets/images/CardInfo1.png';
+import Card2 from '../assets/images/Card2.png';
+import Card2_2 from '../assets/images/Card2-2.png';
+import CardInfo2 from '../assets/images/CardInfo2.png';
+import Card3 from '../assets/images/Card3.png';
+import Card3_3 from '../assets/images/Card3-3.png';
+import CardInfo3 from '../assets/images/CardInfo3.png';
+import Card4 from '../assets/images/Card4.png';
+import Card4_4 from '../assets/images/Card4-4.png';
+import CardInfo4 from '../assets/images/CardInfo4.png';
+import Card5 from '../assets/images/Card5.png';
+import Card5_5 from '../assets/images/Card5-5.png';
+import CardInfo5 from '../assets/images/CardInfo5.png';
+import Card6 from '../assets/images/Card6.png';
+import Card6_6 from '../assets/images/Card6-6.png';
+import CardInfo6 from '../assets/images/CardInfo6.png';
+import Card7 from '../assets/images/Card7.png';
+import Card7_7 from '../assets/images/Card7-7.png';
+import CardInfo7 from '../assets/images/CardInfo7.png';
+import Card8 from '../assets/images/Card8.png';
+import Card8_8 from '../assets/images/Card8-8.png';
+import CardInfo8 from '../assets/images/CardInfo8.png';
+import Card9 from '../assets/images/Card9.png';
+import Card9_9 from '../assets/images/Card9-9.png';
+import CardInfo9 from '../assets/images/CardInfo9.png';
+import Card10 from '../assets/images/Card10.png';
+import Card10_10 from '../assets/images/Card10-10.png';
+import CardInfo10 from '../assets/images/CardInfo10.png';
+import Card11 from '../assets/images/Card11.png';
+import Card11_11 from '../assets/images/Card11-11.png';
+import CardInfo11 from '../assets/images/CardInfo11.png';
+import Card12 from '../assets/images/Card12.png';
+import Card12_12 from '../assets/images/Card12-12.png';
+import CardInfo12 from '../assets/images/CardInfo12.png';
+import Card13 from '../assets/images/Card13.png';
+import Card13_13 from '../assets/images/Card13-13.png';
+import CardInfo13 from '../assets/images/CardInfo13.png';
+import Card14 from '../assets/images/Card14.png';
+import Card14_14 from '../assets/images/Card14-14.png';
+import CardInfo14 from '../assets/images/CardInfo14.png';
+
 
 
 import '../assets/styles/cardinfofigi.css';
@@ -20,61 +52,74 @@ class cardinfofigi extends Component {
     return (
         <div className="contentinfofigi">     
             <div className="contentelements">
-                <img id="imgcardinfo" src={photo1}/>
-                <div className="cardinfo">             
-                </div>
+                <img id="imgcardinfo" src={Card1}/>
+                <img id="imgdecor" src={CardInfo1}/>
+                <img id="imgdecor2" src={Card1_1}/> 
             </div>
             <div className="contentelements2">
-                <div className="cardinfo">         
-                </div>
-                <img id="imgcardinfo" src={photo2}/>
+                <img id="imgdecor2" src={Card2_2}/>
+                <img  id="imgdecor" src={CardInfo2}/>   
+                <img id="imgcardinfo" src={Card2}/>
             </div>
             <div className="contentelements">
-                <img id="imgcardinfo" src={photo3}/>
-                <div className="cardinfo">
-                   
-                </div>
+                <img id="imgcardinfo" src={Card3}/>
+                    <img  id="imgdecor" src={CardInfo3}/>
+                    <img id="imgdecor2" src={Card3_3}/>    
             </div>
             <div className="contentelements2">
-                <div className="cardinfo">                
-                </div>
-                <img id="imgcardinfo" src={photo5}/>
+                 <img id="imgdecor2" src={Card4_4}/>
+                <img  id="imgdecor" src={CardInfo4}/>                
+                <img id="imgcardinfo" src={Card4}/>
             </div>
             <div className="contentelements">
-                <img id="imgcardinfo" src={photo6}/>
-                <div className="cardinfo">
-                    <p>123456</p>                
-                </div>
+                <img id="imgcardinfo" src={Card5}/>
+                <img  id="imgdecor" src={CardInfo5}/>
+                <img id="imgdecor2" src={Card5_5}/>               
             </div>
             <div className="contentelements2">
-                <div className="cardinfo">
-                    <p>123456</p>                
-                </div>
-                <img id="imgcardinfo" src={photo7}/>
+                <img id="imgdecor2" src={Card6_6}/> 
+                <img  id="imgdecor" src={CardInfo6}/>              
+                <img id="imgcardinfo" src={Card6}/>
             </div>
             <div className="contentelements">
-                <img id="imgcardinfo" src={photo8}/>
-                <div className="cardinfo">
-                    <p>123456</p>                
-                </div>
+                <img id="imgcardinfo" src={Card7}/>
+                <img  id="imgdecor" src={CardInfo7}/>
+                <img id="imgdecor2" src={Card7_7}/>                  
             </div>
             <div className="contentelements2">
-                <div className="cardinfo">
-                    <p>123456</p>                
-                </div>
-                <img id="imgcardinfo" src={photo9}/>
+                <img id="imgdecor2" src={Card8_8}/> 
+                <img  id="imgdecor" src={CardInfo8}/>              
+                <img id="imgcardinfo" src={Card8}/>
             </div>
             <div className="contentelements">
-                <img id="imgcardinfo" src={photo10}/>
-                <div className="cardinfo">
-                    <p>123456</p>                
-                </div>
+                <img id="imgcardinfo" src={Card9}/>
+                <img  id="imgdecor" src={CardInfo9}/>
+                <img id="imgdecor2" src={Card9_9}/>                 
             </div>
             <div className="contentelements2">
-                <div className="cardinfo">
-                    <p>123456</p>                
-                </div>
-                <img id="imgcardinfo" src={photo11}/>
+                <img id="imgdecor2" src={Card10_10}/> 
+                <img  id="imgdecor" src={CardInfo10}/>      
+                <img id="imgcardinfo" src={Card10}/>
+            </div>
+            <div className="contentelements">
+            <img id="imgcardinfo" src={Card11}/>
+                <img  id="imgdecor" src={CardInfo11}/>
+                <img id="imgdecor2" src={Card11_11}/>                   
+            </div>
+            <div className="contentelements2">
+                <img id="imgdecor2" src={Card12_12}/>
+                <img  id="imgdecor" src={CardInfo12}/>                 
+                <img id="imgcardinfo" src={Card12}/>
+            </div>
+            <div className="contentelements">
+                <img id="imgcardinfo" src={Card13}/>
+                <img  id="imgdecor" src={CardInfo13}/>
+                <img id="imgdecor2" src={Card13_13}/>             
+            </div>
+            <div className="contentelements2">
+                <img id="imgdecor2" src={Card14_14}/>      
+                <img  id="imgdecor" src={CardInfo14}/>           
+                <img id="imgcardinfo" src={Card14}/>
             </div>
         </div>
     );
